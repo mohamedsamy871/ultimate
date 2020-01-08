90ef2c8a11ec603f4e963a19e1d34f4af9ea1e6a
mohamedsamy871 <47160188+mohamedsamy871@users.noreply.github.com>
2020-01-08 20:09:42 +0200

    sidebar modification

--- Dist/js/main-min.js
+++ Dist/js/main-min.js
@@ -1,1 +1,1 @@
-$(function(){"use strict";$(".toggle-sidebar").on("click",function(){$(".content-area, .sidebar").toggleClass("no-sidebar")})});
+$(function(){"use strict";$(".toggle-sidebar").on("click",function(){$(".content-area, .sidebar").toggleClass("no-sidebar")}),$(".toggle-icon").on("click",function(){$(this).next(".child-links").slideToggle(),$(".fa-angle-right").toggleClass(".down")})});
--- stage/js/main.js
+++ stage/js/main.js
@@ -3,4 +3,9 @@ $(function(){
     $(".toggle-sidebar").on("click",function(){
         $(".content-area, .sidebar").toggleClass("no-sidebar");
     });
+    $('.toggle-icon').on('click',function(){
+        $(this).next(".child-links").slideToggle();
+        $('.fa-angle-right').toggleClass('.down');
+   
+    });
 });
